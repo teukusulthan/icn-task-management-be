bceb527791377702a9e504f5f8a279ae302879fa
teukusulthan <teukusulthan00@gmail.com>
2026-03-14 20:21:59 +0700

    feat(auth-utils): add jwt token generation utility

---
+++ src/utils/jwt.ts
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const generateToken = (userId: string) => {
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
+};
