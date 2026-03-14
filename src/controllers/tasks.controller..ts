897dd5295e7188e5aa66aac277ebbd587b4a06e5
teukusulthan <teukusulthan00@gmail.com>
2026-03-14 20:02:44 +0700

    feat(controllers): add users and tasks controller skeleton

---
+++ src/controllers/tasks.controller..ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const createTask = (req: Request, res: Response) => {};
+export const getTasks = (req: Request, res: Response) => {};
+export const getMyTasks = (req: Request, res: Response) => {};
+export const getTaskById = (req: Request, res: Response) => {};
+export const updateTask = (req: Request, res: Response) => {};
+export const deleteTask = (req: Request, res: Response) => {};
---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const createUser = (req: Request, res: Response) => {};
+export const getUsers = (req: Request, res: Response) => {};
+export const getUserById = (req: Request, res: Response) => {};
+export const updateUser = (req: Request, res: Response) => {};
+export const deleteUser = (req: Request, res: Response) => {};
+export const loginUser = (req: Request, res: Response) => {};
+export const getUserTasks = (req: Request, res: Response) => {};
