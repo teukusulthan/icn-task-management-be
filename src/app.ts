a484ba3be2776175c2e6de3dd5ab192a329c6e5b
teukusulthan <teukusulthan00@gmail.com>
2026-03-14 20:00:50 +0700

    feat(routes): register users and tasks routes in express app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { corsMiddleware } from "./middlewares/cors";
 import cookieParser from "cookie-parser";
+import usersRoutes from "./routes/users.routes";
+import tasksRoutes from "./routes/tasks.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(corsMiddleware);
+
+app.use("/users", usersRoutes);
+app.use("/tasks", tasksRoutes);
 
 export default app;
