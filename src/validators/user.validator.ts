257ec927fe7f3a293da86a9b2ad490c7fdeffbf7
teukusulthan <teukusulthan00@gmail.com>
2026-03-14 20:22:20 +0700

    feat(validation): add login validation schema

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -4,3 +4,8 @@ export const createUserSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
