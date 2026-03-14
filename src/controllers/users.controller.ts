7c4a3e7956d064c4650e2161be0fd70b58a6186e
teukusulthan <teukusulthan00@gmail.com>
2026-03-14 20:21:08 +0700

    feat(users): implement user registration endpoint

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,45 @@
 import { Request, Response } from "express";
+import { prisma } from "../connections/client";
+import { hashPassword } from "../utils/password";
+import { createUserSchema } from "../validators/user.validator";
 
-export const createUser = (req: Request, res: Response) => {};
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const validatedData = createUserSchema.parse(req.body);
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: validatedData.email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already registered",
+      });
+    }
+
+    const hashedPassword = await hashPassword(validatedData.password);
+
+    const user = await prisma.user.create({
+      data: {
+        email: validatedData.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({
+      message: "Invalid request",
+      error,
+    });
+  }
+};
+
 export const getUsers = (req: Request, res: Response) => {};
 export const getUserById = (req: Request, res: Response) => {};
 export const updateUser = (req: Request, res: Response) => {};
